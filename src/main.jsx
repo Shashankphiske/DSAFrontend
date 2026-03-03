987cf5e2d2a727a1da5fd9918da1ee238df1388a
ShashankPhiske <shashankphiske@gmail.com>
2026-03-04 00:24:54 +0530

    sample

--- src/main.jsx
+++ src/main.jsx
@@ -2,33 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import Home from './components/Home.jsx';
-import Navbar from "./components/Navbar.jsx";
-import Layout from './components/Layout.jsx';
-import Features from "./components/Features.jsx"
-import About from "./components/About.jsx"
-import Algorithms from './components/Algorithms.jsx';
-import FAQ from './components/FAQ.jsx';
-import Error from "./components/Error.jsx"
-
-const router = createBrowserRouter([
-  {
-    path : "/",
-    element : <Layout />,
-    children : [
-      { index : true, element : <Home /> },
-      { path : "features", element : <Features /> },
-      { path : "about", element : <About /> },
-      { path : "algo", element : <Algorithms /> },
-      { path : "faq", element : <FAQ /> },
-      { path : "*", element : <Error /> }
-    ]
-  }
-])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
